f55c2095a22f25169ffd7bf3ae951917bfe25419
Keadex <12099977+keadex@users.noreply.github.com>
2021-07-17 00:06:42 +0200

    added microservice and kafka configuration

--- apps/company/src/main.ts
+++ apps/company/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CompanyModule } from './company.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(CompanyModule);
-  await app.listen(3002);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          process.env.COMPANY_MS_KAFKA_HOST +
+            ':' +
+            process.env.COMPANY_MS_KAFKA_PORT,
+        ],
+      },
+    },
+  });
+
+  app.startAllMicroservices();
+  await app.listen(process.env.COMPANY_MS_TCP_PORT);
+  console.log(`Company microservice is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- apps/experience/src/main.ts
+++ apps/experience/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ExperienceModule } from './experience.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(ExperienceModule);
-  await app.listen(3001);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          process.env.EXPERIENCE_MS_KAFKA_HOST +
+            ':' +
+            process.env.EXPERIENCE_MS_KAFKA_PORT,
+        ],
+      },
+    },
+  });
+
+  app.startAllMicroservices();
+  await app.listen(process.env.EXPERIENCE_MS_TCP_PORT);
+  console.log(`Experience microservice is running on: ${await app.getUrl()}`);
 }
 bootstrap();
