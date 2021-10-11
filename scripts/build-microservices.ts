a9905f13b93952c766691401f365d01fe903ca83
Keadex <12099977+keadex@users.noreply.github.com>
2021-10-12 01:39:11 +0200

    added github action for CI/CD

---
+++ scripts/build-microservices.ts
@@ -0,0 +1,7 @@
+import * as shell from 'shelljs';
+import { readdirSync } from 'fs';
+
+readdirSync('./apps/').forEach((microservice) => {
+  console.log(`Building ${microservice} microservice`);
+  shell.exec(`nest build ${microservice}$`);
+});
