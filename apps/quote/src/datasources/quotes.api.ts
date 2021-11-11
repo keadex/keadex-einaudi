19957dcc9f44f1c4849a69ab9c73c501dd4d5322
Keadex <12099977+keadex@users.noreply.github.com>
2021-11-11 17:06:40 +0100

    removed unused code

--- apps/quote/src/datasources/quotes.api.ts
+++ apps/quote/src/datasources/quotes.api.ts
@@ -4,7 +4,6 @@ import { Quotes } from '../models/quotes.model';
 //Thanks to https://github.com/lukePeavey/quotable
 export class QuotesAPI extends RESTDataSource {
   constructor() {
-    // Always call super()
     super();
     // Sets the base URL for the REST API
     this.baseURL = 'https://api.quotable.io/';
@@ -16,13 +15,4 @@ export class QuotesAPI extends RESTDataSource {
       Math.floor(Math.random() * quotes.totalPages) + 1;
     return this.get(`quotes?page=${randomPage}`);
   }
-
-  // async getMostViewedMovies(limit = 10) {
-  //   const data = await this.get('movies', {
-  //     // Query parameters
-  //     per_page: limit,
-  //     order_by: 'most_viewed',
-  //   });
-  //   return data.results;
-  // }
 }
